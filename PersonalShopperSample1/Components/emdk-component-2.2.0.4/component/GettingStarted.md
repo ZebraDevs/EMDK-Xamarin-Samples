b0dc01a0860e08e492a7bc48eb9e9638cb411bc5
Eddie Correia <edward.correia@zebra.com>
2019-07-25 13:23:48 -0600

    upload Personal Shopper sample app for Xamarin 4.0

---
+++ PersonalShopperSample1/Components/emdk-component-2.2.0.4/component/GettingStarted.md
@@ -0,0 +1,53 @@
+
+# EMDK For Xamarin - 2.2
+The EMDK for Xamarin v2.2 provides developers with a comprehensive set of tools to easily create powerful line of business applications for Android enterprise mobile computing devices while retaining their C# skills and tools. The EMDK for Xamarin includes class libraries, sample applications with source code, as well as all of the associated documentation to help your applications take full advantage of what Zebra Technologies devices have to offer.
+
+One unique feature to EMDK for Xamarin is to control device behavior and configuration through the use of profiles. The EMDK Profile Manager lets you create profiles right from your IDE using a GUI interface for selecting the features and settings that your application wishes to use. Then in your application you would simply apply the profile when needed. This results in a simple approach and minimal lines of code required to accomplish tasks resulting in reduced development time, effort and errors.
+
+Features:
+
+* Supports Visual Studio 2013 and greater
+* Supports Xamarin Studio 5.9.4 upto 6.0 (Windows and Mac)
+* Xamarin Studio 6.0 and greater is supported only on Mac
+* Installs as an IDE Plug-in for Visual Studio & Xamarin Studio
+* Online Documentation
+* Sample Code
+* Built-in Barcode API Sample
+* Online Data Capture Profile Sample
+* Online Wi-Fi Profile Sample
+* Symbol.XamarinEMDK APIs
+	* EMDKManager, ProfileManager, VersionManager
+	* Symbol.XamarinEMDK.Barcode
+
+## 1) Complete IDE Setup
+Installing the Xamarin Component only enables the EMDK API library, you must complete the installation of the IDE integration. This step only needs to be performed once, however adding the EMDK For Xamarin component needs to be performed for each project you wish to include it in.
+
+- [Read the Visual Studio Setup Guide](http://techdocs.zebra.com/emdk-for-xamarin/2-2/guide/vs/setup/)
+- [Read the Xamarin Studio Setup Guide](http://techdocs.zebra.com/emdk-for-xamarin/2-2/guide/xs/setup/)
+
+## 2) Try the Sample
+A sample is included as part of the Xamarin Component package to help get you started. Click on the **samples** tab on the component details page to add it to your solution project.
+
+- [Read the Using Xamarin Samples Guide](http://techdocs.zebra.com/emdk-for-xamarin/2-2/samples/)
+
+## 3) Understand Profile Manager
+One unique feature to EMDK for Xamarin is to control device behavior and configuration through the use of *profiles*. The EMDK Profile Manager lets you create profiles right from your IDE using a GUI interface for selecting the features and settings that your application wishes to use. Then in your application you would simply apply the profile when needed. This results in a simple approach and minimal lines of code required to accomplish tasks
+
+- [Read the Profile Manager Overview Guide](http://techdocs.zebra.com/emdk-for-xamarin/2-2/guide/profilemanager/about/)
+
+## 4) Build a Simple Application
+Now that you have your environment setup and have an overview of the EMDK for Xamarin, let's walk through and build an application from scratch.
+
+- [Follow the Hello Xamarin tutorial](http://techdocs.zebra.com/emdk-for-xamarin/2-2/tutorial/helloxamarin/)
+
+## 5) Check out our docs
+We have a lot more resources for you to benefit from:
+
+- API reference
+- Developer Guides
+- Tutorials
+- Videos
+- More Samples
+
+
+- [Check our full online docs](http://techdocs.zebra.com/)
---
+++ PersonalShopperSample1/PersonalShopper1/MainActivity.cs
@@ -0,0 +1,387 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Symbol.XamarinEMDK;
+using Symbol.XamarinEMDK.PersonalShopper;
+using PersonalShopperSample1;
+
+
+namespace EMDKXamarinPersonalShopper
+{
+    [Activity(Label = "EMDKXamarinPersonalShopper", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity, EMDKManager.IEMDKListener
+    {
+        private EMDKManager emdkManager = null;
+        private PersonalShopperMgr PsObject = null;
+        bool mLedsmooth = false;
+        private TextView textViewStatus = null;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            textViewStatus = FindViewById<TextView>(Resource.Id.PStextView);
+
+            EMDKResults results = EMDKManager.GetEMDKManager(ApplicationContext, this);
+            if (results.StatusCode != EMDKResults.STATUS_CODE.Success)
+            {
+                textViewStatus.Text = string.Empty;
+                textViewStatus.SetText("Status: " + "Failed in getEMDKManager::" + results.StatusCode, TextView.BufferType.Normal);
+            }
+            else
+            {
+                textViewStatus.Text = string.Empty;
+                textViewStatus.SetText("Status: " + "getEMDKManager Success", TextView.BufferType.Normal);
+            }
+
+            addCrdInfoButtonListener();
+            addbtnUnlockButtonListener();
+            addFlashLedsButtonListener();
+            addFCCheckboxListener();
+            addDiagnosticButtonListener();
+
+        }
+
+        private void addDiagnosticButtonListener()
+        {
+            Button btnSetCfgLeds = FindViewById<Button>(Resource.Id.Diagnosticdata);
+            btnSetCfgLeds.Click += DiagnosticButton_Click;
+        }
+
+        private void DiagnosticButton_Click(object sender, EventArgs e)
+        {
+            textViewStatus.Text = string.Empty;
+            getDiagnosticData();
+        }
+
+        private void getDiagnosticData()
+        {
+            DiagnosticData diagnosticData = null;
+            DiagnosticParamId diagnosticparamID = new DiagnosticParamId();
+            int paramId = DiagnosticParamId.All;
+
+            DiagnosticConfig diagnosticconfig = new DiagnosticConfig(200, 60);
+
+            if (null != PsObject.Diagnostic)
+            {
+                try
+                {
+                    diagnosticData = PsObject.Diagnostic.GetDiagnosticData(paramId, diagnosticconfig);
+                }
+                catch (DiagnosticException e)
+                {
+                    e.PrintStackTrace();
+                    textViewStatus.SetText("Status: " + e.Message, TextView.BufferType.Normal);
+                }
+                if (diagnosticData != null)
+                {
+                    showMessage("\n Battery Capacity in Per : " + diagnosticData.BatteryStateOfCharge
+                                + "\n Battery Capacity in mins: " + diagnosticData.BatteryTimeToEmpty
+                                + "\n Battery SOH in Per: " + diagnosticData.BatteryStateOfHealth
+                                + "\n Battery Charging Time Required mins: " + diagnosticData.BatteryChargingTime
+                                + "\n Battery Replacement in days: " + diagnosticData.TimeSinceBatteryReplaced
+                                + "\n Time since Last reboot  mins: " + diagnosticData.TimeSinceReboot
+                                + "\n Battery Charging Time Elapsed mins: " + diagnosticData.BatteryChargingTimeElapsed
+                                + "\n Manufacturing Date: " + diagnosticData.BatteryDateOfManufacture, ToastLength.Long
+                            );
+                }
+            }
+
+        }
+
+        private void addSmmothCheckboxListener()
+        {
+		    if(null!=PsObject.Cradle){
+                    CheckBox mFCState = FindViewById<CheckBox>(Resource.Id.checkBox1);
+                    mLedsmooth = mFCState.Checked;
+                }
+        }
+
+        private void addFCCheckboxListener()
+        {
+            CheckBox mFCState = FindViewById<CheckBox>(Resource.Id.ChecBoxfastcharge);
+            
+            mFCState.Click += mFCState_Click;
+        }
+
+        private void mFCState_Click(object sender, EventArgs e)
+        {
+            textViewStatus.Text = string.Empty;
+            if (null != PsObject.Cradle)
+            {
+                try
+                {
+                    if (((CheckBox)(sender)).Checked)
+                    {
+                        PsObject.Cradle.Config.SetFastChargingState(true);
+                        if (PsObject.Cradle.Config.FastChargingState)
+                        {
+                            textViewStatus.Text = string.Empty;
+                            textViewStatus.SetText("Status: " + "fast charge enabled", TextView.BufferType.Normal);
+                        }
+                        else
+                        {
+                            textViewStatus.Text = string.Empty;
+                            textViewStatus.SetText("Status: " + "fast charge enabling failed", TextView.BufferType.Normal);
+                        }
+                    }
+                    else
+                    {
+                        PsObject.Cradle.Config.SetFastChargingState(false);
+                        if (!(PsObject.Cradle.Config.FastChargingState))
+                        {
+                            textViewStatus.Text = string.Empty;
+                            textViewStatus.SetText("Status: " + "fast charge disabled", TextView.BufferType.Normal);
+                        }
+                        else
+                        {
+                            textViewStatus.Text = string.Empty;
+                            textViewStatus.SetText("Status: " + "fast charge disabling failed", TextView.BufferType.Normal);
+                        }
+                    }
+                }
+                catch (CradleException ex)
+                {
+                    ex.PrintStackTrace();
+                    textViewStatus.Text = string.Empty;
+                    textViewStatus.SetText("Status: " + ex.Message, TextView.BufferType.Normal);
+                }
+            }
+
+        }
+
+
+        public void showMessage(String text)
+        {
+            Toast toast = Toast.MakeText(ApplicationContext, text, ToastLength.Long);
+            toast.Show();
+        }
+
+        public void showMessage(String text, ToastLength duration)
+        {
+            Toast toast = Toast.MakeText(ApplicationContext, text, duration);
+            toast.Show();
+        }
+
+        protected void OnDestroy()
+        {
+            base.OnDestroy();
+            disable();
+            if (PsObject != null)
+            {
+                PsObject = null;
+            }
+
+            if (emdkManager != null)
+            {
+                emdkManager.Release();
+                emdkManager = null;
+            }
+        }
+
+        public void OnClosed()
+        {
+            emdkManager.Release();
+        }
+
+        void addFlashLedsButtonListener()
+        {
+            Button btnFlashLeds = FindViewById<Button>(Resource.Id.FlashLEDButton1);
+            btnFlashLeds.Click += BtnFlashLeds_Click;
+
+        }
+
+        private void BtnFlashLeds_Click(object sender, EventArgs e)
+        {
+            textViewStatus.Text = string.Empty;
+            flashLeds();
+        }
+
+        private void flashLeds()
+        {
+            if (null != PsObject.Cradle)
+            {
+                int onDuration = 2000;
+                int offDuration = 1000;
+                int flashCount = 5;
+
+                try
+                {
+                    addSmmothCheckboxListener();
+                    CradleLedFlashInfo ledFlashInfo = new CradleLedFlashInfo(onDuration, offDuration, mLedsmooth);
+                    CradleResults result = PsObject.Cradle.FlashLed(flashCount, ledFlashInfo);
+                    if (result == CradleResults.Success)
+                    {
+                        textViewStatus.Text = string.Empty;
+                        textViewStatus.SetText("Status: " + "Flashed LEDs ", TextView.BufferType.Normal);
+                    }
+                    else
+                    {
+                        textViewStatus.Text = string.Empty;
+                        textViewStatus.SetText("Status: " + "Failed error " + result.Description, TextView.BufferType.Normal);
+                    }
+                }
+                catch (CradleException e)
+                {
+                    // TODO Auto-generated catch block
+                    e.PrintStackTrace();
+                    textViewStatus.Text = string.Empty;
+                    textViewStatus.SetText("Status: " + e.Message, TextView.BufferType.Normal);
+                }
+            }
+        }
+
+        private void addbtnUnlockButtonListener()
+        {
+            Button btnUnlock = FindViewById<Button>(Resource.Id.UnlockButton1);
+            btnUnlock.Click += BtnUnlock_Click;
+            
+        }
+
+        private void BtnUnlock_Click(object sender, EventArgs e)
+        {
+            textViewStatus.Text = string.Empty;
+            unlock();
+        }
+
+        private void unlock()
+        {
+            if (null != PsObject.Cradle)
+            {
+                int onDuration = 500;
+                int offDuration = 500;
+                int unlockDuration = 15;
+
+                try
+                {
+                    addSmmothCheckboxListener();
+                    CradleLedFlashInfo ledFlashInfo = new CradleLedFlashInfo(onDuration, offDuration, mLedsmooth);
+                    CradleResults result = PsObject.Cradle.Unlock(unlockDuration, ledFlashInfo);
+                    if (result == CradleResults.Success)
+                    {
+                        textViewStatus.Text = string.Empty;
+                        textViewStatus.SetText("Status: " + "Unlocked", TextView.BufferType.Normal);
+                    }
+                    else
+                    {
+                        textViewStatus.Text = string.Empty;
+                        textViewStatus.SetText("Status: " + "Failed error " + result.Description, TextView.BufferType.Normal);
+                    }
+                }
+                catch (CradleException e)
+                {
+                    // TODO Auto-generated catch block
+                    e.PrintStackTrace();
+                    textViewStatus.Text = string.Empty;
+                    textViewStatus.SetText("Status: " + e.Message, TextView.BufferType.Normal);
+                }
+            }
+        }
+
+        private void addCrdInfoButtonListener()
+        {
+            Button btnCrdInfo = FindViewById<Button>(Resource.Id.CradleInfoButton1);
+            btnCrdInfo.Click += BtnCrdInfo_Click;
+	    }
+
+        private void BtnCrdInfo_Click(object sender, EventArgs e)
+        {
+            textViewStatus.Text = string.Empty;
+            getCradleInfo();
+        }
+
+        private void getCradleInfo()
+        {
+            CradleInfo cradleInfo = null;
+            if (null != PsObject.Cradle)
+            {
+                try
+                {
+                    cradleInfo = PsObject.Cradle.CradleInfo;
+                }
+                catch (CradleException e)
+                {
+                    e.PrintStackTrace();
+                    textViewStatus.Text = string.Empty;
+                    textViewStatus.SetText("Status: " + e.Message, TextView.BufferType.Normal);
+                }
+                if (cradleInfo != null)
+                {
+                 //   Log.d("PartNo", "part No=" + cradleInfo.getPartNumber());
+                    showMessage("FirmwareVersion: " + cradleInfo.FirmwareVersion
+                                + "\nDateOfManufacturing: " + cradleInfo.DateOfManufacture
+                                + "\nHardwareID: " + cradleInfo.HardwareID
+                                + "\nPartnumber: " + cradleInfo.PartNumber
+                                + "\nSerialNumber: " + cradleInfo.SerialNumber, ToastLength.Long
+                            );
+                }
+            }
+        }
+
+        protected void disable()
+        {
+            try
+            {
+                if (null != PsObject.Cradle)
+                {
+                    PsObject.Cradle.Disable();
+                }
+            }
+            catch (CradleException e)
+            {
+                // TODO Auto-generated catch block
+                e.PrintStackTrace();
+                textViewStatus.Text = string.Empty;
+                textViewStatus.SetText("Status: " + e.Message, TextView.BufferType.Normal);
+            }
+        }
+
+        protected void enable()
+        {
+            try
+            {
+                if (null != PsObject.Cradle)
+                {
+                    PsObject.Cradle.Enable();
+                }
+            }
+            catch (CradleException e)
+            {
+                // TODO Auto-generated catch block
+                e.PrintStackTrace();
+                textViewStatus.Text = string.Empty;
+                textViewStatus.SetText("Status: " + e.Message, TextView.BufferType.Normal);
+            }
+        }
+
+        public void OnOpened(EMDKManager emdkManager)
+        {
+            this.emdkManager = emdkManager;
+            try
+            {
+                PsObject = (PersonalShopperMgr)this.emdkManager.GetInstance(Symbol.XamarinEMDK.EMDKManager.FEATURE_TYPE.Personalshopper);
+            }
+            catch (Exception e)
+            {
+                textViewStatus.Text = string.Empty;
+                textViewStatus.SetText("Status: " + e.Message, TextView.BufferType.Normal);
+            }
+
+            if (PsObject == null)
+            {
+                textViewStatus.Text = string.Empty;
+                textViewStatus.SetText("Status: " + "PersonalShopper NOT Feature supported", TextView.BufferType.Normal);
+            }
+            else
+            {
+                enable();
+            }
+        }
+    }
+}
+
